155bad6968bfea7e5fa2f102596b10aad6f6d7fc
styxnix <jaakko.saarikko@protonmail.com>
2023-10-11 10:52:50 +0300

    laitteet listätty ja laiteosoitetta ei voit jättää tyhjäksi

--- hallintaapp/models.py
+++ hallintaapp/models.py
@@ -14,7 +14,7 @@ class Device(models.Model):
     ip_address = models.GenericIPAddressField()
     name = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
-    admin_link = models.URLField()
+    admin_link = models.URLField(blank=True, null=True)
     click_count = models.PositiveIntegerField(default=0)
     
     def __str__(self):
